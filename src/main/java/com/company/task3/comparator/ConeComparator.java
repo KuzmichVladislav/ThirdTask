ad43aefcf4eaa35932723c5086f1041369c6131c
Kuziatihii <61865176+Kuziatihii@users.noreply.github.com>
2021-06-28 14:58:42 +0300

    Add observer

---
+++ src/main/java/com/company/task3/comparator/ConeComparator.java
@@ -0,0 +1,19 @@
+package com.company.task3.comparator;
+
+import com.company.task3.entity.Cone;
+
+import java.util.Comparator;
+
+public enum ConeComparator {
+    ID,
+    POINT,
+    RADIUS,
+    HEIGHT,
+    SURFACE_AREA,
+    GENERATRIX_LENGTH;
+
+    public Comparator<Cone> comparator() {
+        return
+    }
+
+}
